2c8a3a57793b1a3f3c21a966851ad5e0cf0d7415
Magnus Holm <judofyr@gmail.com>
2022-08-06 17:44:41 +0200

    Implement DictArray

---
+++ src/DictArray.zig
@@ -0,0 +1,49 @@
+//! DictArray stores a list of integers by placing the unique items in a separate
+//! array and refering to indexes into that array.
+
+const std = @import("std");
+const CompactArray = @import("./CompactArray.zig");
+
+const Self = @This();
+
+dict: CompactArray,
+arr: CompactArray,
+
+pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+    self.dict.deinit(allocator);
+    self.arr.deinit(allocator);
+    self.* = undefined;
+}
+
+pub fn bits(self: *const Self) usize {
+    return self.dict.bits() + self.arr.bits();
+}
+
+pub fn get(self: *const Self, idx: usize) u64 {
+    return self.dict.get(self.arr.get(idx));
+}
+
+pub fn encode(allocator: std.mem.Allocator, data: []const u64) !Self {
+    var dict = std.ArrayList(u64).init(allocator);
+    defer dict.deinit();
+
+    var arr = try std.ArrayList(u64).initCapacity(allocator, data.len);
+    defer arr.deinit();
+
+    var mapping = std.hash_map.AutoHashMap(u64, usize).init(allocator);
+    defer mapping.deinit();
+
+    for (data) |val| {
+        var result = try mapping.getOrPut(val);
+        if (!result.found_existing) {
+            result.value_ptr.* = dict.items.len;
+            try dict.append(val);
+        }
+        try arr.append(result.value_ptr.*);
+    }
+
+    return Self{
+        .dict = try CompactArray.encode(allocator, dict.items),
+        .arr = try CompactArray.encode(allocator, arr.items),
+    };
+}
