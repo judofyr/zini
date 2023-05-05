a76109ece50e636e0d1dc838d864f3bc9b3e7b29
Magnus Holm <judofyr@gmail.com>
2023-05-05 22:37:11 +0200

    Add kcov support

--- build.zig
+++ build.zig
@@ -16,6 +16,16 @@ pub fn build(b: *std.build.Builder) void {
     const tests_run_step = b.addRunArtifact(tests);
     tests_run_step.has_side_effects = true;
 
+    const coverage = b.option(bool, "test-coverage", "Generate test coverage") orelse false;
+    if (coverage) {
+        tests_run_step.argv.insertSlice(0, &[_]std.Build.RunStep.Arg{
+            .{ .bytes = b.dupe("kcov") },
+            .{ .bytes = b.dupe("--include-path") },
+            .{ .bytes = b.dupe(".") },
+            .{ .bytes = b.dupe("coverage") }, // output dir
+        }) catch unreachable;
+    }
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&tests_run_step.step);
 
